4e8d71521c52f22d895386b641bbd47b8dbb57fb
soverby <shane.overby@greatwide-tm.com>
2018-08-21 23:27:23 -0400

    updated project to use webpack to generate modular dependency bundle javascript file; moved previously used app.js to playground directory as moving forward each component of the Indecision app will be modularized

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,11 @@
+// entry point
+// output file
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
