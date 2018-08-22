0e611a6487b6f097713c66bb7ee2fe0c764e9bfe
soverby <shane.overby@greatwide-tm.com>
2018-08-21 23:49:44 -0400

    intro to importing and exporting via webpack; added person.js and utils.js to test exporting their members; importing in app.js

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -65,9 +65,37 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is running!!!!!!!!!!!!!!!!');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person__ = __webpack_require__(2);
+// //import './utils.js'
+
+// import { square, add } from './utils';
+
+// console.log('app.js is running!!!!!!!!!!!!!!!!');
+// console.log(square(4));
+// console.log(add(12, 54));
+
+
+
+console.log(`Is an adult: ${__WEBPACK_IMPORTED_MODULE_0__person__["b" /* isAdult */](33)}`);
+console.log(`Can drink alcohol: ${__WEBPACK_IMPORTED_MODULE_0__person__["a" /* canDrink */](12)}`);
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = (age) => age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+
+const canDrink = (age) => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 /***/ })
 /******/ ]);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running!!!!!!!!!!!!!!!!');
+// //import './utils.js'
+
+// import { square, add } from './utils';
+
+// console.log('app.js is running!!!!!!!!!!!!!!!!');
+// console.log(square(4));
+// console.log(add(12, 54));
+
+import { isAdult, canDrink } from './person';
+
+console.log(`Is an adult: ${isAdult(33)}`);
+console.log(`Can drink alcohol: ${canDrink(12)}`);
