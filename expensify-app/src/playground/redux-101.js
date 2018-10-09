e9f7cdecc175d59c51f1585887dde4fe6f588a6d
soverby <shane.overby@greatwide-tm.com>
2018-10-09 13:15:05 -0400

    Added properties to some of the action objects being passed as arguments to the redux store's dispatch method; added logic to account for these possible properties in the switch cases

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -3,23 +3,32 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
     switch(action.type){
         case 'INCREMENT':
+            const incrementBy =  typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         default:
             return state;
     }
 });
 
-console.log(store.getState());
+// store.subscribe() gets called anytime the state managed by the redux store changes and returns a function that can be invoked to unsubscribe from the store's state changes ----- see unsubscribe use below...
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
 // Actions - an object that gets sent to the store
 
@@ -27,8 +36,12 @@ console.log(store.getState());
 
 // I'd like to increment the count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
+
+// calling unsubscribe to stop logging the state to the console (all that's being done in the store's subscribe callback function)
+//unsubscribe();
 
 store.dispatch({
     type: 'INCREMENT'
@@ -44,5 +57,12 @@ store.dispatch({
     type: 'DECREMENT'
 });
 
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
