f7d81dc37f7bddc6c8fabd8affeccca710723e13
soverby <shane.overby@greatwide-tm.com>
2018-10-10 12:00:40 -0400

    Created reducers for each root property of the demoState object: expenses, filters; each of these reducers has it's own default state object; using redux's  combineReducers method to create the redux store with more than one reducer function; also added redux devtools store enhancer as an argument to createStore, this enables the redux devtools extension that I have installed in Chrome

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,52 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Creating reducers for each root property in state: expenses, filters
+
+// Expenses reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filters reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+};
+
+// Store creation
+
+const store = createStore(combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'sdfkjsdflk',
