1d025be715ebbb587deda1cee273d24b81f24606
soverby <shane.overby@greatwide-tm.com>
2018-10-09 23:18:42 -0400

    Added ES6 array destructuring examples to es6-destructuring.js; created increment, decrement, reset, and set action generators to redux-101.js

--- expensify-app/src/playground/es6-destructuring.js
+++ expensify-app/src/playground/es6-destructuring.js
@@ -1,3 +1,5 @@
+// object destructuring
+
 const person = {
     name: 'Shane',
     age: 32,
@@ -13,10 +15,12 @@ const { name: firstname = 'Anonymous', age } = person;
 
 console.log(`${firstname} is ${age}.`);
 
-const { temp: temperature, city } = person.location;
+const { temp: temperature, city1 } = person.location;
 
-if(temperature && city)
-    console.log(`It's ${temperature} in ${city}.`);
+if(temperature && city1)
+    console.log(`It's ${temperature} in ${city1}.`);
+
+// challenge:
 
 const book = {
     title: 'Ego is the Enemy',
@@ -29,3 +33,33 @@ const book = {
 const { name: publisherName = 'Self-Published' } = book.publisher;
 
 console.log(publisherName);
+
+
+// array destructuring
+
+const address = [
+    '123 Super Duper Avenue',
+    'Chicago',
+    'Illinois',
+    '23455'
+];
+
+const [, city2 = 'Mocksville', state = 'North Carolina' ] = address;
+
+// obviously  not intuitive as to what these indexed address values are...
+console.log(`Your are in ${address[1]}, ${address[2]}.`);
+
+console.log(`Your are in ${city2}, ${state}.`);
+
+// challenge:
+
+const item = [
+    'Coffee (hot)',
+    '$2.00',
+    '$2.50',
+    '$2.75'
+];
+
+const [ beverage, , mediumPrice ] = item;
+
+console.log(`A medium ${beverage} costs ${mediumPrice}.`);
--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,16 +1,35 @@
 import { createStore } from 'redux';
 
+// Action generators = functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+const setCount = (newCount) => ({
+    type: 'SET',
+    newCount
+});
+
 const store = createStore((state = { count: 0 }, action) => {
     switch(action.type){
         case 'INCREMENT':
-            const incrementBy =  typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'RESET':
             return {
@@ -35,34 +54,23 @@ const unsubscribe = store.subscribe(() => {
 // increment, decrement, reset
 
 // I'd like to increment the count
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
 // calling unsubscribe to stop logging the state to the console (all that's being done in the store's subscribe callback function)
 //unsubscribe();
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
 // I'd like to reset the count to zero
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(resetCount());
 
 // I'd like to decrement the count
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
+store.dispatch(decrementCount(10));
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount(101));
