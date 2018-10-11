27e0074feb8156290d80d6e15137b60de2a1f67a
soverby <shane.overby@greatwide-tm.com>
2018-10-11 15:44:45 -0400

    Refactored all actions, reducers, store configuration, etc. from redux-expenses.js into separate files and directories; redux is now setup to integrate with the expensify components

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import getVisibleExpenses from './selectors/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 7500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 12000 }));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('gas'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ expensify-app/src/reducers/expenses.js
@@ -0,0 +1,25 @@
+// Expenses reducer
+
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        case 'ADD_EXPENSE':
+            return [...state, action.expense];
+        case 'REMOVE_EXPENSE':
+            return state.filter(e => e.id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((e) => {
+                if(e.id == action.id) {
+                    return {
+                        ...e,
+                        ...action.updates
+                    };
+                }
+                return e;
+            });
+        default:
+            return state;
+    }
+};
+
