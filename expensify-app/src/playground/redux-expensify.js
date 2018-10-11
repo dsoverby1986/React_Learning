026f8ba3a02c36ee8545fe0c93bc34f90c768ee6
soverby <shane.overby@greatwide-tm.com>
2018-10-11 10:05:37 -0400

    Added the editExpense, setTextFilter, sortByDate, and sortByAmount action generators; updated the expense and filter reducers to include cases for these added actions; installed babel plugin to provide object spreading ES6 feature

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -23,13 +23,32 @@ const addExpense = (
 // REMOVE_EXPENSE
 const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
-    expenseId: id
+    id
 });
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
 // SET_START_DATE
 // SET_END_DATE
 
@@ -44,7 +63,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
         case 'ADD_EXPENSE':
             return [...state, action.expense];
         case 'REMOVE_EXPENSE':
-            return state.filter(e => e.id !== action.expenseId);
+            return state.filter(e => e.id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((e) => {
+                if(e.id == action.id) {
+                    return {
+                        ...e,
+                        ...action.updates
+                    };
+                }
+                return e;
+            });
         default:
             return state;
     }
@@ -61,6 +90,21 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch(action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
         default: 
             return state;
     }
@@ -82,6 +126,14 @@ const expense2 = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }
 
 store.dispatch(removeExpense({ id: expense1.expense.id }));
 
+store.dispatch(editExpense(expense2.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
+
 const demoState = {
     expenses: [{
         id: 'sdfkjsdflk',
@@ -97,4 +149,3 @@ const demoState = {
         endDate: undefined
     }
 };
-
