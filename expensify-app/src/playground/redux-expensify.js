21ba12163d9ce45e274ec32fa6a3d8ceb846905b
soverby <shane.overby@greatwide-tm.com>
2018-10-11 09:25:40 -0400

    Created addExpense and removeExpense action generators and registered those actions as cases in the expenseReducer; dispatching two addExpense actions and one removeExpense action

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,31 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    expenseId: id
+});
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -17,6 +41,10 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type) {
+        case 'ADD_EXPENSE':
+            return [...state, action.expense];
+        case 'REMOVE_EXPENSE':
+            return state.filter(e => e.id !== action.expenseId);
         default:
             return state;
     }
@@ -45,7 +73,14 @@ const store = createStore(combineReducers({
     filters: filtersReducer
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expense1 = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expense2 = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expense1.expense.id }));
 
 const demoState = {
     expenses: [{
