13d40ef7b1e6f0c0cd1e8ae5421752d4c3ba0286
soverby <shane.overby@greatwide-tm.com>
2018-10-12 15:15:17 -0400

    Installed the react-redux package to connect the react application components to the redux data store; wrapped the AppRouter component in application root with Provider component provided by react-redux, to expose the redux store to the application components; created HOC ExpenseList using the connect function provided by react-redux to specify what data from the redux store to expose to the component via it's props object; imported and using the ExpenseList component in the ExpenseDashboardPage component; played around with higher order components in hoc.js

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
@@ -22,4 +23,14 @@ store.dispatch(setTextFilter('bill'));
 store.dispatch(setTextFilter('gas'));
 store.dispatch(setTextFilter('water'));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
